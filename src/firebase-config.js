7adc0c50cdb30f384963e3cf73c55bc702173a38
srivz <srivathsananim@gmail.com>
2022-11-30 09:58:21 +0530

    modified:   src/firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmd9K0gqjhqdkyAAiKxa0oLPOoI4RKxbc",
-  authDomain: "clearhire-16fa0.firebaseapp.com",
-  databaseURL: "https://clearhire-16fa0-default-rtdb.firebaseio.com",
-  projectId: "clearhire-16fa0",
-  storageBucket: "clearhire-16fa0.appspot.com",
-  messagingSenderId: "270898440669",
-  appId: "1:270898440669:web:56c3e85932b7bfe39cee34",
+  apiKey: "AIzaSyACghDZdyBg3lnZ5fIpz80OvaWkVU859LE",
+  authDomain: "clearhire-dd29e.firebaseapp.com",
+  projectId: "clearhire-dd29e",
+  storageBucket: "clearhire-dd29e.appspot.com",
+  messagingSenderId: "469717688214",
+  appId: "1:469717688214:web:482bb1306e9f974ac287b7",
+  measurementId: "G-814TXP49NH",
 };
 
 export const fire = initializeApp(firebaseConfig);
